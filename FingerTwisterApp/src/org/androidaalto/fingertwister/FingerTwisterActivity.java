cfa4cddf7b66c3463a9f6d7ebdcadd174035ab1b
Ken <wsd.2012.ken@gmail.com>
2012-06-16 13:50:51 +0300

    button and animation improve

--- FingerTwisterApp/src/org/androidaalto/fingertwister/FingerTwisterActivity.java
+++ FingerTwisterApp/src/org/androidaalto/fingertwister/FingerTwisterActivity.java
@@ -1,5 +1,6 @@
 package org.androidaalto.fingertwister;
 
+
 import org.androidaalto.fingertwister.UserEvent.UserState;
 
 import android.app.Activity;
@@ -11,7 +12,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -21,7 +26,7 @@ public class FingerTwisterActivity extends Activity implements UserEventCallback
     static final int WIN = 1;
 
     private GamePanel gamePane;
-
+    private TextView colorTextView = null;
     /**
      * Called when the activity is first created.
      */
@@ -117,10 +122,12 @@ public class FingerTwisterActivity extends Activity implements UserEventCallback
 		case Proceeding:
 			GamePanel.Instruction currentInstruction = gamePane
 					.getCurrentInstruction();
-
-			TextView colorTextView = (TextView) findViewById(R.id.instruction_color);
+			colorTextView = (TextView) findViewById(R.id.instruction_color);
+			applyRotation(1, 0, 90);
 			colorTextView.setBackgroundColor(currentInstruction.color);
-
+			
+			
+			
 			TextView fingerTextView = (TextView) findViewById(R.id.instruction_finger);
 
 			switch (currentInstruction.finger) {
@@ -182,5 +189,90 @@ public class FingerTwisterActivity extends Activity implements UserEventCallback
 		return dialog;
 	}
 
+    /**
+     * Setup a new 3D rotation on the container view.
+     * @author Google Sample APIs
+     * @param position the item that was clicked to show a picture, or -1 to show the list
+     * @param start the start angle at which the rotation must begin
+     * @param end the end angle of the rotation
+     */
+    private void applyRotation(int position, float start, float end) {
+        // Find the center of the container
+        final float centerX = colorTextView.getWidth() / 2.0f;
+        final float centerY = colorTextView.getHeight() / 2.0f;
 
+        // Create a new 3D rotation with the supplied parameter
+        // The animation listener is used to trigger the next animation
+        final Rotate3dAnimation rotation =
+                new Rotate3dAnimation(start, end, centerX, centerY, 310.0f, true);
+        rotation.setDuration(500);
+        rotation.setFillAfter(true);
+        rotation.setInterpolator(new AccelerateInterpolator());
+        rotation.setAnimationListener(new DisplayNextView(position));
+
+        colorTextView.startAnimation(rotation);
+    }
+    
+
+    /**
+     * This class listens for the end of the first half of the animation. 
+     * It then posts a new action that effectively swaps the views when the container 
+     * is rotated 90 degrees and thus invisible. 
+     * @author Google Sample APIs
+     */
+    private final class DisplayNextView implements Animation.AnimationListener {
+        private final int mPosition;
+        
+        private DisplayNextView(int position) {
+            mPosition = position;
+        }
+
+        public void onAnimationStart(Animation animation) {
+        }
+
+        public void onAnimationEnd(Animation animation) {
+        	colorTextView.post(new SwapViews(mPosition));
+        }
+
+        public void onAnimationRepeat(Animation animation) {
+        }
+    }
+
+    /**
+     * This class is responsible for swapping the views and start the second 
+     * half of the animation. 
+     * @author Google Sample APIs
+     */
+    private final class SwapViews implements Runnable {
+        private final int mPosition;
+        
+        public SwapViews(int position) {
+            mPosition = position;
+        }
+
+        public void run() {
+            final float centerX = colorTextView.getWidth() / 2.0f;
+            final float centerY = colorTextView.getHeight() / 2.0f;
+            Rotate3dAnimation rotation;
+            
+            if (mPosition > -1) {
+
+                rotation = new Rotate3dAnimation(90, 180, centerX, centerY, 310.0f, false);
+            } else {
+
+            	//colorTextView1.setVisibility(View.VISIBLE);
+            	//colorTextView2.setVisibility(View.GONE);
+                //mapview.setVisibility(View.VISIBLE);
+                //mapview.requestFocus();
+
+                rotation = new Rotate3dAnimation(90, 0, centerX, centerY, 310.0f, false);
+            }
+
+            rotation.setDuration(500);
+            rotation.setFillAfter(true);
+            rotation.setInterpolator(new DecelerateInterpolator());
+
+            colorTextView.startAnimation(rotation);
+        }
+    }
 }
